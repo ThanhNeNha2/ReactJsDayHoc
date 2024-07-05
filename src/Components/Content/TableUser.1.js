96539b857c429d386b747e624976dc8b1c8b6469
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-07-05 22:05:41 +0700

    loading no 20 phut

--- src/Components/Content/TableUser.1.js
+++ src/Components/Content/TableUser.1.js
@@ -3,35 +3,34 @@ import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { toast } from "react-toastify";
-
+import { FadeLoader } from "react-spinners";
+import "./TableUser.scss";
 export default class TableUser extends Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
       isLoading: true,
-      error: null,
+      error: false,
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     setTimeout(async () => {
       try {
         let res = await axios.get(`https://reqres.in/api/users?page=1`);
-
         this.setState({
           users: res.data.data,
           isLoading: false,
-          // giả vờ res.total_pages rồi hỏi
-          totalPages: res.data.total_pages,
         });
       } catch (error) {
+        console.log(error);
         this.setState({
-          error: error,
+          error: true,
           isLoading: false,
         });
       }
-    }, 10);
+    }, 500);
   }
 
   handleDelete = () => {
@@ -49,44 +48,60 @@ export default class TableUser extends Component {
               <th>Length</th>
               <th>Gmail</th>
               <th>Avata</th>
-
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.users.map((item, index) => {
-              return (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.email}</td>
-                  <td>{item.first_name}</td>
-                  <td>{item.last_name}</td>
-                  <td>
-                    <img
-                      src={item.avatar}
-                      onClick={() =>
-                        this.setState({
-                          isOpen: true,
-                          photoIndex: item.avatar,
-                        })
-                      }
-                    />
-                  </td>
-                  <td>
-                    {" "}
-                    <button
-                      onClick={() => {
-                        this.handleDelete();
-                      }}
-                      className="btn btn-success"
-                    >
-                      Delete
-                    </button>{" "}
-                    <Button variant="warning">Update</Button>{" "}
-                  </td>
-                </tr>
-              );
-            })}
+            {this.state.error ? (
+              "data is error "
+            ) : this.state.isLoading === true ? (
+              <div className="hihi">
+                <FadeLoader
+                  loading={true}
+                  color="#2bf401"
+                  margin={2}
+                  height={15}
+                  width={5}
+                  radius={2}
+                />{" "}
+              </div>
+            ) : (
+              <>
+                {this.state.users.map((item, index) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.email}</td>
+                      <td>{item.first_name}</td>
+                      <td>{item.last_name}</td>
+                      <td>
+                        <img
+                          src={item.avatar}
+                          onClick={() =>
+                            this.setState({
+                              isOpen: true,
+                              photoIndex: item.avatar,
+                            })
+                          }
+                        />
+                      </td>
+                      <td>
+                        {" "}
+                        <button
+                          onClick={() => {
+                            this.handleDelete();
+                          }}
+                          className="btn btn-success"
+                        >
+                          Delete
+                        </button>{" "}
+                        <Button variant="warning">Update</Button>{" "}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </>
+            )}
           </tbody>
         </Table>
       </div>
